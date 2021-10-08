1bb0bebd68fcd86673ab3eb7f751cfa9182b73b9
huseen shawqi <lmlk.h@mail.ru>
2021-10-09 01:09:57 +0300

    task_three

--- bin/dart_course.dart
+++ bin/dart_course.dart
@@ -1,9 +1,16 @@
-void main() {
-  List<int> a = [5, 10, 15, 20, 25];
-  List b = getFirstAndLastNumber(a);
-  print(b);
+class Person {
+  final String name;
+  final int age;
+  final double height;
+  Person({required this.name, required this.age, required this.height});
+  String printDescription() {
+    return ("My name is $name. I'm $age years old, I'm $height meters tall.");
+  }
 }
 
-List getFirstAndLastNumber(List a) {
-  return [a.first, a.last];
+void main() {
+  Person p1 = Person(name: "hussen", age: 22, height: 1.88);
+  Person p2 = Person(name: "ahmed", age: 26, height: 1.79);
+  print(p1.printDescription());
+  print(p2.printDescription());
 }
